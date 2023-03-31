f0bab5f241352e4e457a420ac3d4ac17f21b5f5a
axel olea <axelcedilloolea@gmail.com>
2023-03-30 20:31:18 -0600

    First Commit (Clase 1 - Segundo Modulo)
    Se creo la clase Dinosaurio
    Se creo la clase obstacle y la heredo la clase cactus

---
+++ dino_runner/components/cactus.py
@@ -0,0 +1,7 @@
+from dino_runner.components.obstacle import Obstacle
+
+class Cactus(Obstacle):
+
+    def __init__(self):
+        super().__init__()
+        pass
---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,16 @@
+from dino_runner.utils.constants import RUNNING, RUNNING_SHIELD, RUNNING_HAMMER
+
+
+class Dinosaur:
+
+    def __init__(self):
+        pass
+    
+    def jump(self):
+        pass
+
+    def run(self):
+        pass
+
+    def duck(self):
+        pass
